0fa95944467a9db5f426ab529eb5e44445bfe5b9
Quentin Pigné <quentin.pigne@gmail.com>
2021-06-19 21:18:11 +0200

    beginning of theming setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+import "!style-loader!css-loader!sass-loader!../projects/ui-lib/src/core/themes/base-theme.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
