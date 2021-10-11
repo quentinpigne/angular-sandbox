67ca75d4de048339c43a35247367e33edf5d27fb
Quentin Pigné <quentin.pigne@gmail.com>
2021-10-11 23:22:52 +0200

    fixing storybook global theme importing issue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,13 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
-import "!style-loader!css-loader!sass-loader!../projects/ui-lib/src/core/theming/themes/base-theme.scss";
+import { componentWrapperDecorator } from '@storybook/angular';
+
+import baseTheme from "../projects/ui-lib/src/core/theming/themes/base-theme.scss";
+
+export const decorators = [
+  componentWrapperDecorator((story) => `<style>${baseTheme}</style>${story}`),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
