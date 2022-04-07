fcd43681f49a738e9fdcb87c24f8a71bdab21982
Quentin Pigné <quentin.pigne@gmail.com>
2022-04-08 00:19:51 +0200

    adding afterClosed observable to return modal data to component

--- projects/ui-lib/src/modal/modal-ref.ts
+++ projects/ui-lib/src/modal/modal-ref.ts
@@ -1,9 +1,17 @@
+import { Observable, Subject } from 'rxjs';
+
 import { OverlayRef } from '../core/overlay/overlay-ref';
 import { GlobalPositionStrategy } from '../core/overlay/position/global-position-strategy';
 
 import { ModalContainerComponent } from './modal-container.component';
 
-export class ModalRef<T> {
+export class ModalRef<T, R> {
+  private _afterClosed: Subject<R | undefined> = new Subject<R | undefined>();
+
+  get afterClosed(): Observable<R | undefined> {
+    return this._afterClosed.asObservable();
+  }
+
   constructor(
     private _overlayRef: OverlayRef,
     private _containerInstance: ModalContainerComponent,
@@ -14,7 +22,9 @@ export class ModalRef<T> {
     });
   }
 
-  close(): void {
+  close(data?: R): void {
+    this._afterClosed.next(data);
+    this._afterClosed.complete();
     this._overlayRef.detach();
   }
 
--- projects/ui-lib/src/modal/modal.service.ts
+++ projects/ui-lib/src/modal/modal.service.ts
@@ -13,7 +13,7 @@ import { ModalContainerComponent } from './modal-container.component';
   providedIn: 'root',
 })
 export class ModalService implements OnDestroy {
-  private _openModal: ModalRef<unknown> | null = null;
+  private _openModal: ModalRef<unknown, unknown> | null = null;
 
   constructor(private readonly _overlayService: OverlayService) {}
 
@@ -21,11 +21,11 @@ export class ModalService implements OnDestroy {
     this._openModal?.close();
   }
 
-  open<T>(componentType: Type<T>, config?: ModalConfig): ModalRef<T> {
+  open<T, R>(componentType: Type<T>, config?: ModalConfig): ModalRef<T, R> {
     const overlayRef: OverlayRef = this._createOverlay(config);
     const modalContainer: ModalContainerComponent = this._attachModalContainer(overlayRef);
-    const modalRef: ModalRef<T> = this._attachModal(overlayRef, modalContainer, componentType);
-    this._openModal = modalRef;
+    const modalRef: ModalRef<T, R> = this._attachModal<T, R>(overlayRef, modalContainer, componentType);
+    this._openModal = modalRef as ModalRef<unknown, unknown>;
     return modalRef;
   }
 
@@ -48,13 +48,13 @@ export class ModalService implements OnDestroy {
     return modalContainerRef.instance;
   }
 
-  private _attachModal<T>(
+  private _attachModal<T, R>(
     overlayRef: OverlayRef,
     modalContainer: ModalContainerComponent,
     componentType: Type<T>,
-  ): ModalRef<T> {
+  ): ModalRef<T, R> {
     const modalContentRef: ComponentRef<T> = modalContainer.attach(componentType);
-    const modalRef: ModalRef<T> = new ModalRef(overlayRef, modalContainer, modalContentRef.instance);
+    const modalRef: ModalRef<T, R> = new ModalRef(overlayRef, modalContainer, modalContentRef.instance);
     modalRef.updatePosition();
     return modalRef;
   }
