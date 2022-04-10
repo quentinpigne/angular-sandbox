e94a4aab81678754534f15160140a2b5ddfdb896
Quentin Pigné <quentin.pigne@gmail.com>
2022-04-10 19:55:38 +0200

    fixing unit tests compilation

--- projects/ui-lib/src/tooltip/tooltip.directive.spec.ts
+++ projects/ui-lib/src/tooltip/tooltip.directive.spec.ts
@@ -1,8 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TooltipDirective } from './tooltip.directive';
 
 describe('TooltipDirective', () => {
+  let directive: TooltipDirective;
+  let fixture: ComponentFixture<TooltipDirective>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TooltipDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TooltipDirective);
+    directive = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new TooltipDirective();
     expect(directive).toBeTruthy();
   });
 });
